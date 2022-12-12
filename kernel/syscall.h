00e55175604009120283d55863ffe0b75faae09b
蓝桥 <381602449@qq.com>
2022-12-13 00:06:11 +0800

    信号量--睡眠唤醒

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -24,8 +24,8 @@
 #define SYS_sh_var_read   22
 #define SYS_sh_var_write  23
 #define SYS_sem_create    24
-#define SYS_sem_free      24
-#define SYS_sem_wait      24
-#define SYS_sem_signal    24
+#define SYS_sem_free      25
+#define SYS_sem_wait      26
+#define SYS_sem_signal    27
 
 
--- user/sync_test.c
+++ user/sync_test.c
@@ -4,23 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-    int id = sem_create(1);
+    int id = 1;
+    id = sem_create(1);
+    printf("%d\n", id);
+    
     int pid = fork(); 
     int status, i;   
 
-    for(i = 0; i < 100000; i++)
+    for(i = 0; i < 10000; i++)
     {   
         sem_wait(id);
         sh_var_write();
         sem_signal(id);
     }
-    
-    printf("pid%d: result = %d\n", pid, sh_var_read());
+       
     if(pid > 0)
     {
         wait(&status);   
         sem_free(id);
+        printf("pid%d: result = %d\n", pid, sh_var_read());
     }   
+    else
+    {
+        printf("pid%d: result = %d\n", pid, sh_var_read());
+    }
 
     exit(0);
 }
